87db9e607906b9c1e74436d715fc6a92cf35f337
philxavier <fsxlima@gmail.com>
2022-07-23 15:05:02 -0400

    creates initial grid

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import { Card } from './Card';
-import { cities } from '../MockData/Cities';
 import css from './grid.module.scss';
+
+const cities = [
+  'Fort Francisca',
+  'Washington',
+  'Lake Aron',
+  'West Leslie',
+  'New Hunterville',
+  'Hiramview',
+  'Lake Dulce',
+  'Leuschkeside',
+  'West Cristinahaven',
+  'South Dextershire',
+  'Cummerataburgh',
+  'Ogden',
+  'Newark',
+  'Urbana',
+  'North Rahulmouth',
+  'Considineville',
+  'Port Torrance',
+  'Fort Danika',
+  'Genevievemouth',
+  'East Alexandreashire',
+  'Elnafurt',
+  'Amiyaburgh',
+  'Lynn',
+  'South Marquis',
+  'Zboncakfield',
+  'Bechtelarfield',
+  'Fort Lyda',
+  'Ellicott City',
+  'Mistymouth',
+  'Sayreville',
+  'Weissnatside'
+];
 export function Grid() {
+  const [cityData, setCityData] = React.useState(cities);
+
   return (
-    <div className={`mt-32 px-16 ${css.grid}`}>
+    <div className={`mt-32 ${css.grid}`}>
       {cities.map((city) => {
         return <Card city={city} />;
       })}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { Grid } from '../Components/Grid';
 const Index = () => {
   return (
-    <div>
+    <div className='flex justify-center px-8 '>
+      {/* <div>test</div>
+      <div>test</div> */}
       {/* <Header/>
     <ViewControllers/> */}
       <Grid />
